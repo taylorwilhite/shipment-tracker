5955e94c219fc40c268cda73f357f81f4b6f9a99
Taylor Wilhite <taylorwilhite@gmail.com>
2018-11-07 13:51:08 -0800

    Scaffold script logic

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
+
 const app = express();
 require('dotenv').config();
+
+// Overall Function
+const getUnshipped = () => {
+  // Check new shipments
+  const newShipments = getNewShipments();
+  // Submit new shipments to list
+  addToShipmentList(newShipments);
+  // Pull entire list to be checked
+  const shipmentList = getShipmentList();
+  // Get Tracking info on shipments and update list
+  updateList(trackShipments(shipmentList));
+  // Return new unshipped order list
+  const lateOrders = getUnshippedOrders();
+  return lateOrders;
+};
+
+// Every 24 hours run the function
+getUnshipped();
 
 app.listen(process.env.PORT || 8008, () => {
   console.log('app is running!');
